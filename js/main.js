bce069d144eaa85a7bde0cdeecb3f3c009e28006
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2021-03-30 13:29:20 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+/*MENU DE NAVEGACION*/
+const menu = document.querySelector('.menuNavegacion');
+const linkContainer = document.querySelector('.contenedorLista');
+const main = document.querySelector('#main');
+const pegar = menu.offsetTop;
+const btnMenu = document.querySelector('.btnMenu');
+let on_off = true;
+
+window.onscroll = () => {
+
+	//MENU PEGADO
+	if (window.pageYOffset >= pegar) {
+       menu.classList.add('pegar');
+       main.style.marginTop = '170px';
+    }else{
+        menu.classList.remove('pegar');
+        main.style.marginTop = '100px'; 
+    }
+
+    //HOME
+    if (window.pageYOffset >= 500) {
+    	document.querySelector('.home').style.right = '0%';
+    }else{
+    	document.querySelector('.home').style.right = '-100%';
+    }
+
+    document.querySelector('.home').addEventListener('click', () => {
+    	document.body.scrollTop = '0';
+    	document.documentElement.scrollTop = '0';
+    });
+}
+
+//MENU RESPONSIVO
+btnMenu.addEventListener('click', () => {
+	if (on_off) {
+		linkContainer.style.left = '0';
+
+		on_off = false;
+	}else{
+		on_off = false
+		linkContainer.style.left = '-100%';
+
+		on_off = true;
+	}
+});
+
+//GALERIA	
+const images = document.querySelectorAll('.imgCard img');
+const modal = document.querySelector('galeriaModal');
+const imgModal = document.querySelector('#imgModal');
+const cierreModal = document.querySelector('.cierreModal')
+
+
