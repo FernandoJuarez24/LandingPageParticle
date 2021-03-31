5804663fff954076ff511cebc80d52df9b348c0a
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2021-03-31 12:52:20 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -44,10 +44,30 @@ btnMenu.addEventListener('click', () => {
 	}
 });
 
-//GALERIA	
+//GALERIA MODAL
 const images = document.querySelectorAll('.imgCard img');
-const modal = document.querySelector('galeriaModal');
+const modal = document.querySelector('.galeriaModal');
 const imgModal = document.querySelector('#imgModal');
-const cierreModal = document.querySelector('.cierreModal')
+const cierreModal = document.querySelector('.cierreModal');
+const captionModal = document.querySelector('.captionModal');
+const listaGaleria = document.querySelectorAll('.listaGaleria');
+
+images.forEach((images) => {
+	images.addEventListener('click', () => {
+		modal.style.display = 'block';
+		imgModal.src = images.src;
+		captionModal.innerHTML = images.alt;
+	});
+});
+
+listaGaleria.forEach(img  => img.addEventListener('click', lightbox));
+	function lightbox  (event){
+		imgModal.src = event.target.src;
+		captionModal.innerHTML = event.target.alt;
+	}
+
+	cierreModal.addEventListener('click', () => {
+    modal.style.display = "none";
+});
 
 
